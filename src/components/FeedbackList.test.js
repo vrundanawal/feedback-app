6220e4184648bfe650c1d654afdde47ce0b46f54
vrundanawal <nawal.vrunda@gmail.com>
2022-10-27 11:39:31 +0100

    Updated with some more test cases

--- src/components/FeedbackList.test.js
+++ src/components/FeedbackList.test.js
@@ -4,6 +4,7 @@ import FeedbackList from "./FeedbackList";
 //import userEvent from "@testing-library/user-event";
 import { shallow } from "enzyme";
 import FeedbackData from "../data/FeedbackData";
+import { render, screen } from "@testing-library/react";
 
 describe("<FeedbackList/>", () => {
   it("should render the feedback List component", () => {
@@ -14,4 +15,16 @@ describe("<FeedbackList/>", () => {
 test("should test TodoLists component with default state of array", () => {
   const wrapper = shallow(<FeedbackList feedback={FeedbackData} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test("It should show the message if there is no Feedback data available", () => {
+  render(<FeedbackList feedback={[]} />);
+  const message = screen.getByTestId("error-message");
+  expect(message).toBeInTheDocument();
+});
+
+test("It will return the jsx if length is 0", () => {
+  render(<FeedbackList feedback={FeedbackData.length === 0} />);
+  const message = screen.getByTestId("error-message");
+  expect(message).toBeInTheDocument();
 });
